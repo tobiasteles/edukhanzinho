1e056392f9824eb0705c53456e29595530631c65
Tobias Teles <tobias.teles@gmail.com>
2025-07-29 19:21:19 -0300

    Update header.tsx

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -9,7 +9,7 @@ export const Header = () => {
         <header className="h-20 w-full border-b-2 border-slate-200 px-4">
             <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full ">
         <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
-            <Image src="/SELO/SELO 2.png" height={50} width={50} alt="Logo"/>
+            <Image src="/SELO/SELO 2.png" height={50} width={50} alt="Logo" className="hidden sm:block"/>
             <h1 className="text-2xl font-extrabold text-blue-700 tracking-wide">Edukhanzinho</h1>
         </div>
         <ClerkLoading>
