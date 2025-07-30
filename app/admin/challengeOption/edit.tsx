591c1470ed2f899ab7f0698a417c601ee103ac40
Tobias Teles <tobias.teles@gmail.com>
2025-07-30 19:35:31 -0300

    Update admin page

--- app/admin/challengeOption/edit.tsx
+++ app/admin/challengeOption/edit.tsx
@@ -3,6 +3,7 @@ import {
   Edit,
   ReferenceInput,
   required,
+  SelectInput,
   SimpleForm,
   TextInput,
 } from "react-admin";
@@ -12,21 +13,16 @@ export const ChallengeOptionEdit = () => {
     <Edit>
       <SimpleForm>
         <TextInput source="text" validate={[required()]} label="Texto" />
-        <BooleanInput
-        source="correct"
-        label="Opção Correta"
-        />
-        <ReferenceInput source="challengeId" reference="challenges" label="Questão"/>
-       <TextInput
-       source="imageSrc"
-       validate={[required()]}
-       label="Image"
-       />
-       <TextInput
-       source="audioSrc"
-       validate={[required()]}
-       label="Audio"
-       />
+        <BooleanInput source="correct" label="Opção Correta" />
+        <ReferenceInput
+          source="challengeId"
+          reference="challenges"
+          label="Questão"
+        >
+          <SelectInput optionText="question" validate={[required()]} />
+        </ReferenceInput>
+        <TextInput source="imageSrc" validate={[required()]} label="Imagem" />
+        <TextInput source="audioSrc" validate={[required()]} label="Áudio" />
       </SimpleForm>
     </Edit>
   );
