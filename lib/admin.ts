f7af3abbb34cf08f7e8191714b486b1897f26c1a
Tobias Teles <106561230+tobiasteles@users.noreply.github.com>
2025-08-11 14:59:53 -0300

    Atualizar o admin.ts

--- lib/admin.ts
+++ lib/admin.ts
@@ -4,6 +4,8 @@ const adminIds = [
   "user_2zTiwmeur0umKHBR2Ae8AD8vDmt",
   "user_30ZT38hHqzHxEgQDA5tVYYli7fG",
   "user_30eqtuQWSDZlDBcT9ryWUGXrSh6",
+"user_30YUqbde2BlVZmPVMvQouBIStOR",
+"user_319PHfKsljZJCY5tcIeeG8GSbF9",
 ];
 
 export const isAdmin = async () => {
