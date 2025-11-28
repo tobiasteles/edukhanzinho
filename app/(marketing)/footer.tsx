7e6f83fa0abc21e9a82ae1be5507266f91590b2f
Tobias Teles <tobias.teles@gmail.com>
2025-11-27 21:28:42 -0300

    Update footer.tsx

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -6,7 +6,7 @@ export const Footer = () => {
         <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
             <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
             <Button size="lg" variant="ghost" className="flex-1">
-                <Image src="/A.png" alt="A" height={32} width={40}  className="mr-4 rounded-md"/>
+                <Image src="/a.png" alt="A" height={32} width={40}  className="mr-4 rounded-md"/>
             </Button>
             <Button size="lg" variant="ghost" className="flex-1">
                 <Image src="/E.png" alt="E" height={32} width={40}  className="mr-4 rounded-md"/>
