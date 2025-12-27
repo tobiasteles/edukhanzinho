ec131f3357a5d3c6c86e9409cd3283ad53b25555
Tobias Teles <tobias.teles@gmail.com>
2025-12-27 16:15:23 -0300

    Update schema.ts

--- db/schema.ts
+++ db/schema.ts
@@ -9,6 +9,24 @@ import {
   timestamp,
 } from "drizzle-orm/pg-core";
 
+export const userProgress = pgTable("user_progress", {
+  userId: text("user_id").primaryKey(),
+  userName: text("user_name").notNull().default("User"),
+  userImageSrc: text("user_image_src").notNull().default("/user.png"),
+  activeCourseId: integer("active_course_id").references(() => courses.id, {
+    onDelete: "cascade",
+  }),
+  hearts: integer("hearts").notNull().default(5),
+  points: integer("points").notNull().default(0),
+});
+
+export const userProgressRelations = relations(userProgress, ({ one }) => ({
+  activeCourse: one(courses, {
+    fields: [userProgress.activeCourseId],
+    references: [courses.id],
+  }),
+}));
+
 export const courses = pgTable("courses", {
   id: serial("id").primaryKey(),
   title: text("title").notNull(),
@@ -116,23 +134,7 @@ export const challengeProgressRelations = relations(
   })
 );
 
-export const userProgress = pgTable("user_progress", {
-  userId: text("user_id").primaryKey(),
-  userName: text("user_name").notNull().default("User"),
-  userImageSrc: text("user_image_src").notNull().default("/user.png"),
-  activeCourseId: integer("active_course_id").references(() => courses.id, {
-    onDelete: "cascade",
-  }),
-  hearts: integer("hearts").notNull().default(5),
-  points: integer("points").notNull().default(0),
-});
 
-export const userProgressRelations = relations(userProgress, ({ one }) => ({
-  activeCourse: one(courses, {
-    fields: [userProgress.activeCourseId],
-    references: [courses.id],
-  }),
-}));
 
 export const userSubscription = pgTable("user_subscription", {
   id: serial("id").primaryKey(),
